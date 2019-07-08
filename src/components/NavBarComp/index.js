0e166707675c4a7bc559b5567907797636031b8b
Adliano Alves <adliano@me.com>
2019-07-07 19:09:49 -0700

    changed navbar component

--- src/components/NavBarComp/index.js
+++ src/components/NavBarComp/index.js
@@ -2,25 +2,37 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
-
+// Navbar component
 export function NavBarComp (props) {
   return (
-    <Navbar bg='secondary' variant='dark' expand="lg">
-      <Navbar.Brand href='/' className='text-light'><h2>{props.applicationName}</h2></Navbar.Brand>
+    <Navbar bg='secondary' variant='dark' expand='lg' fixed='top'>
+      <Navbar.Brand href='/' className='text-light'>
+        <h2>{props.applicationName}</h2>
+      </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Navbar.Text className='mx-auto text-light'>
-          <h2>Click an Image to begin</h2>
+          <h2>{props.text}</h2>
         </Navbar.Text>
         <Nav variant='pills'>
-          <Nav.Item className='text-light bg-success p-3 m-1 rounded-pill'>
-            <h3>Score: { props.score || 0 }</h3>
-          </Nav.Item>
-          <Nav.Item className='text-light bg-primary p-3 m-1 rounded-pill'>
-            <h3>Top Score: { props.topScore || 0 }</h3>
-          </Nav.Item>
+          <NavPill text='Score' bg='bg-success'>
+            {props.score || 0}
+          </NavPill>
+          <NavPill text='Top Score' bg='bg-primary'>
+            {props.Topscore || 0}
+          </NavPill>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
+  )
+}
+// Navbar Pill used to display scores
+export function NavPill (props) {
+  return (
+    <Nav.Item className={`text-light p-1 m-1 rounded-pill ${props.bg}`}>
+      <h3 className='px-4'>
+        {props.text}: {props.children}
+      </h3>
+    </Nav.Item>
   )
 }
