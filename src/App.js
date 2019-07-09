426cf3d2388f56d5136f236a044ba50e29bf3fb0
Adliano Alves <adliano@me.com>
2019-07-08 23:54:23 -0700

    score and topscore now updating

--- src/App.js
+++ src/App.js
@@ -24,63 +24,62 @@ class App extends Component {
    */
   reloadGame = () => {
     fetch('assets/data/data.json')
-      .then((response) => response.json())
-      .then((results) => {
+      .then(response => response.json())
+      .then(results => {
         this.setState({ characters: results })
       })
-      .catch((err) => console.log(err))
+      .catch(err => console.log(err))
   }
   /**
    * componentDidMount
    * This method is called after components where mount
    */
   componentDidMount () {
-    this.reloadGame()    
+    this.reloadGame()
   }
   /**
    * OnCardClick()
    */
   // TODO: Implement this method
-  OnCardClick = (event) => {
+  OnCardClick = event => {
     // Save the instance of the clicked element
     const elementClicked = event.target
-    //// DEBUGGING \\\\
+    /// / DEBUGGING \\\\
     console.log(elementClicked)
-    // Check 
+    // Check
     // if  card clicked == true reset score to 0 and reload game
     if (this.state.characters[elementClicked.id].clicked) {
-      console.log('game over');
+      console.log('game over')
       // Reset score
-      this.setState({ score: 0})
+      this.setState({ score: 0 })
       // Reload Game
       this.reloadGame()
-    }
-    else {
-      // Update character clicked to true 
-      this.setState({ characters: this.state.characters.map((element, index) => {
-        // typeof index Output 'number' and 
-        // typeof elementClicked.id Output 'string'
-        // Therefor i used parseInt() to void warning about using `==`
-        if(index === parseInt(elementClicked.id)) {
-          return {...element, clicked: true}
-        }
-        else return {...element}
-      })})
-      // Update Score
-      this.setState({ score: (this.state.score + 1) })
+    } else {
+      // Update character clicked to true
+      this.setState({
+        characters: this.state.characters.map((element, index) => {
+          // typeof index Output 'number' and
+          // typeof elementClicked.id Output 'string'
+          // Therefor i used parseInt() to void warning about using `==`
+          if (index === parseInt(elementClicked.id)) {
+            return { ...element, clicked: true }
+          } else return { ...element }
+        })
+      })
       // Check if topscore have been reach
-      if(this.state.score > this.state.topscore) {
-        this.setState({ topscore: this.state.score})
+      if (this.state.score >= this.state.topscore) {
+        this.setState({
+          score: this.state.score + 1,
+          topscore: this.state.score + 1
+        })
+      } else {
+        // Update Score
+        this.setState({ score: this.state.score + 1 })
       }
-      
-      // TODO: re-render card randonly 
+      // TODO: re-render card randonly
     }
-    
+
     console.log(this.state.characters[elementClicked.id])
-
-    // Also need to check if top score was reached
-    // TODO: Remove Debugging line inside renderCharacters() Method
-
   }
   /**
    * renderCharacters()
@@ -90,13 +89,18 @@ class App extends Component {
    */
   renderCharacters = () => {
     let _characters = this.state.characters.map((character, index) => (
-      <Image src={character.image} key={index} id={index} onClick={this.OnCardClick}/>
+      <Image
+        src={character.image}
+        key={index}
+        id={index}
+        onClick={this.OnCardClick}
+      />
     ))
 
     /// DEBUGGING \\\
     // Used to show on console if state `clicked` was change to true
     console.log(this.state.characters)
-    
+
     // Return Arrya with <Image/>
     return _characters
   }
@@ -128,7 +132,7 @@ Adding Assets Outside of the Module System
 https://facebook.github.io/create-react-app/docs/using-the-public-folder
 In this approach assets/images/ will be placed at public/ dir and
 we can use fetch() to access it.
-also we can use `process.env.PUBLIC_URL`, `window.location` to access 
+also we can use `process.env.PUBLIC_URL`, `window.location` to access
 images from assets/images/
 
 sample of character json
@@ -137,16 +141,16 @@ sample of character json
 }
 */
 
- // if (!this.state.characters[elementClicked.id].clicked) {
-    //   this.setState({ characters: this.state.characters.map((element, index) => {
-    //     // console.log(typeof index)
-    //     // Output 'number' and 
-    //     // console.log(typeof elementClicked.id)
-    //     // Output 'string'
-    //     // Therefor i used parseInt() to void warning about using `==`
-    //     if(index === parseInt(elementClicked.id)) {
-    //       return {...element, clicked: true}
-    //     }
-    //     else return {...element}
-    //   })})
-    // }
+// if (!this.state.characters[elementClicked.id].clicked) {
+//   this.setState({ characters: this.state.characters.map((element, index) => {
+//     // console.log(typeof index)
+//     // Output 'number' and
+//     // console.log(typeof elementClicked.id)
+//     // Output 'string'
+//     // Therefor i used parseInt() to void warning about using `==`
+//     if(index === parseInt(elementClicked.id)) {
+//       return {...element, clicked: true}
+//     }
+//     else return {...element}
+//   })})
+// }
