ebc90ac62392537eb1b55d53087377fa65186c08
Adliano Alves <adliano@me.com>
2019-07-12 22:16:28 -0700

    fixed the header's text alingment

--- src/components/HeaderComp/index.js
+++ src/components/HeaderComp/index.js
@@ -6,8 +6,8 @@ import './style.css'
 
 export default function HeaderComp(props) {
     return (
-        <Jumbotron fluid className='jumbotron text-light text-center'>
-            <Container className='m-5 p-5'>
+        <Jumbotron fluid className='jumbotron text-light'>
+            <Container className='my-5 py-5 mx-auto' >
                 <h1>Memory Game</h1>
                 <h3>Click on an image to earn points, but don't click on any more than once!</h3>
             </Container>
